176242342dadd7aba891705c204204350d0d6085
SaberBerrehili <sabercool04@gmail.com>
2026-03-15 00:35:05 +0100

    feat: fetch NBA games by date and display results

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from balldontlie import BalldontlieAPI
+import sys
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+if API_KEY is None : 
+    sys.exit("Missing API KEY")
+api = BalldontlieAPI(api_key=API_KEY)
+
+games = api.nba.games.list(dates = ["2025-03-14"])
+for game in games.data : 
+    winner,winner_score, loser, loser_score = (game.home_team.full_name,game.home_team_score,game.visitor_team.full_name, game.visitor_team_score) if (game.home_team_score > game.visitor_team_score) else (game.visitor_team.full_name,game.visitor_team_score,game.home_team.full_name,game.home_team_score)
+    print(f"{winner} won ({int(winner_score)} - {int(loser_score)}) against {loser} ")
+    
+
